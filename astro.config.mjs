5cc3922fdf91177e38b704bd3a2c6be126941651
Kartavya Vashishtha <sendtokartavya@gmail.com>
2026-06-01 11:12:31 -0700

    good font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, config } from 'astro/config';
 import { generateDzi } from './src/plugins/2025-postlaunch-dzi.mjs';
 
 export default defineConfig({
@@ -7,6 +7,19 @@ export default defineConfig({
     '/': '/2026-prelaunch',
     '/pages': '/2025-postlaunch/pages',
   },
+  fonts: [
+    {
+      name: 'SourceSerif4',
+      src: './src/fonts/SourceSerif4-VariableFont_opsz,wght.ttf',
+      weight: '200 900',
+    },
+    {
+      name: 'SourceSerif4',
+      src: './src/fonts/SourceSerif4-Italic-VariableFont_opsz,wght.ttf',
+      weight: '200 900',
+      style: 'italic',
+    },
+  ],
   vite: {
     ssr: { external: ['p5'] },
   },
